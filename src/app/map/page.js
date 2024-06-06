84720c2cc1259acea51cf23177a15405bf2f9b6c
steampunkWizard <108240403+SteampunkWizardStudios@users.noreply.github.com>
2024-06-06 16:24:32 +0000

    add loading placeholder for map & animation

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -4,12 +4,19 @@ import dynamic from "next/dynamic";
 import markers from "./markers.json";
 import InfoBox from "../components/infoBox.js";
 import TaskBar from "../components/taskBar.js";
+import styles from "../styles/main.module.css";
 
 const InteractiveMap = dynamic(
   () => import("../components/interactiveMap.js"),
   {
     ssr: false, // server side rendering is disabled
-    loading: () => <h1>Loading map...</h1>,
+    loading: () => 
+    <div className={styles.loading}>
+      <h1>
+        Loading map
+      </h1>
+    </div>
+    ,
   }
 );
 
