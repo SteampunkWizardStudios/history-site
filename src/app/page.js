899545583aa4370862c9bb948cb880b1080cfbc2
steampunkWizard <108240403+SteampunkWizardStudios@users.noreply.github.com>
2024-06-10 14:23:20 +0000

    added credits

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ export default function Home() {
   return (
     <>
       <Question />
+      <h1 className={styles.credit}>By: Elijah N.</h1>
+      <h2 className={styles.credit2}>Group: Gabriel V, Zachary W, Owen R</h2>
       <TaskBar />
     </>
   );
